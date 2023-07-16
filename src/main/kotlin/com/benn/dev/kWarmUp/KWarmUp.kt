dcd44268f0e58a3d4124b30297e2c6e85489f825
Dev-Beom <youngbeom0908@gmail.com>
2023-07-16 23:23:42 +0900

    feat annotation retention runtime set

--- src/main/kotlin/com/benn/dev/kWarmUp/KWarmUp.kt
+++ src/main/kotlin/com/benn/dev/kWarmUp/KWarmUp.kt
@@ -4,4 +4,5 @@ package com.benn.dev.kWarmUp
  * @param count The number of counts the target method is executed
  */
 @Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
 annotation class KWarmUp(val count: Int)
