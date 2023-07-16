9036222f855961f873bb73d14718e36c652d5836
Dev-Beom <youngbeom0908@gmail.com>
2023-07-17 01:05:25 +0900

    set EnableKWarmUp class and factories

--- src/main/kotlin/com/benn/dev/kWarmUp/EnableKWarmUp.kt
+++ src/main/kotlin/com/benn/dev/kWarmUp/EnableKWarmUp.kt
@@ -3,6 +3,6 @@ package com.benn.dev.kWarmUp
 import org.springframework.context.annotation.Import
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.TYPE)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS)
 @Import(KWarmUpBeanRegistrationProcessor::class)
 annotation class EnableKWarmUp
