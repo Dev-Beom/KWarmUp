089ffddcdb6146c7bde27e6afaacb5545af61fc9
Dev-Beom <youngbeom0908@gmail.com>
2023-07-18 18:31:20 +0900

    docs

--- src/main/kotlin/com/benn/dev/kWarmUp/KWarmUp.kt
+++ src/main/kotlin/com/benn/dev/kWarmUp/KWarmUp.kt
@@ -1,7 +1,7 @@
 package com.benn.dev.kWarmUp
 
 /**
- * @param count The number of counts the target method is executed
+ * @param repeatCount The number of counts the target method is executed
  */
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
